7db0fb5beeaabc81822eb2644697fb7b00d38317
Martijn Poppen <martijn.poppen@kpn.com>
2022-03-30 00:03:49 +0200

    NEW: actions and conditions

--- app.js
+++ app.js
@@ -1,21 +1,26 @@
-"use strict";
+'use strict';
 
-const Homey = require("homey");
+const Homey = require('homey');
+const flowActions = require('./lib/flows/actions');
+const flowConditions = require('./lib/flows/conditions');
 
 class App extends Homey.App {
-  log() {
-    console.log.bind(this, "[log]").apply(this, arguments);
-  }
+    log() {
+        console.log.bind(this, '[log]').apply(this, arguments);
+    }
 
-  error() {
-    console.error.bind(this, "[error]").apply(this, arguments);
-  }
+    error() {
+        console.error.bind(this, '[error]').apply(this, arguments);
+    }
 
-  // -------------------- INIT ----------------------
+    // -------------------- INIT ----------------------
 
-  async onInit() {
-    this.log(`${this.homey.manifest.id} - ${this.homey.manifest.version} started...`);
-  }
+    async onInit() {
+        this.log(`${this.homey.manifest.id} - ${this.homey.manifest.version} started...`);
+
+        await flowActions.init(this.homey);
+        await flowConditions.init(this.homey);
+    }
 }
 
 module.exports = App;

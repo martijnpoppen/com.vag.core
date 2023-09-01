4bde31173f1b9fe2e476c244d83ace70907fd81d
Martijn Poppen <m.poppen@ziggo.nl>
2023-09-01 23:13:24 +0200

    FIX: lib

--- drivers/main-driver.js
+++ drivers/main-driver.js
@@ -193,11 +193,9 @@ module.exports = class mainDriver extends Homey.Driver {
                 ctx.homey.app.log(`[Driver] ${ctx.id} - ctx._weConnectClient.getState() - try: ${i}`);
                 ctx.homey.app.log(`[Driver] ${ctx.id} - info.connection - `, weConnectData['info.connection']);
 
-                if (i > 5 && weConnectData['info.connection']) {
+                if (retry === 7) {
                     await ctx._weConnectClient.onUnload(() => {});
                     return Promise.resolve(weConnectData);
-                } else if (retry === 9) {
-                    return Promise.resolve(false);
                 }
             }
 
